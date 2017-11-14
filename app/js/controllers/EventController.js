5a86b515c4f46dcbdfada4ac12f355c164d68a26
Remus Sinorchian <rsinorch@techteam.com>
2017-11-14 22:54:26 +0200

    - add node backend to server http get/post for events - add http service in angular app, to get the event data from the node backend

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,35 +1,12 @@
 'use strict';
 
-eventsApp.controller("EventController",
-    function EventController($scope) {
-        $scope.event = {
-            name: "Angular js training",
-            date: "01/01/2017",
-            location: {
-                city: "Bucharest",
-                country: "Romania"
-            },
-            sessions: [
-                {
-                    name: "Intro to angular",
-                    hour: "9:30",
-                    room: "Vlad Tepes",
-                    upvote: 0
-                },
-                {
-                    name: "Angular new features",
-                    hour: "12:30",
-                    room: "Mihai Viteazul",
-                    upvote: 0
-                },
-                {
-                    name: "Angular 1.5 new features",
-                    hour: "14:00",
-                    room: "Matei Basarab",
-                    upvote: 0
-                }
-            ]
-        }
+eventsApp.controller('EventController',
+    function EventController($scope, eventData) {
+        // $scope.event = eventData.event;
+
+        eventData.getEvent(function(event) {
+            $scope.event = event;
+        })
 
         $scope.upVoteSession = function(session) {
             session.upvote++;
