d4f0d307f9424d99da9e19755ef216f490787b9f
Remus Sinorchian <rsinorch@techteam.com>
2017-11-14 23:15:43 +0200

    small change how the http service is used

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,18 +1,19 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function EventController($scope, eventData) {
-        // $scope.event = eventData.event;
-
-        eventData.getEvent(function(event) {
+    function EventController($scope, $log, eventData) {
+        eventData.getEvent().success(function (event) {
             $scope.event = event;
-        })
+        }).error(function (data, status, headers, config) {
+            $log.warn(data, status, headers(), config);
+        });
 
-        $scope.upVoteSession = function(session) {
+        $scope.upVoteSession = function (session) {
             session.upvote++;
-        }
+        };
 
-        $scope.downVoteSession = function(session) {
+        $scope.downVoteSession = function (session) {
             session.upvote--;
-        }
-    });
+        };
+    })
+;
--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,45 +1,9 @@
 'use strict';
 
-eventsApp.factory('eventData', function ($http, $log) {
+eventsApp.factory('eventData', function ($http) {
     return {
-        getEvent: function (successCallBack) {
-            $http({method: 'GET', url: '/data/event/1'})
-                .success(function (data, status, header, config) {
-                    successCallBack(data);
-                })
-                .error(function (data, status, header, config) {
-                    $log.warn(data, status, headers(), config);
-                });
-
+        getEvent: function () {
+            return $http({method: 'GET', url: '/data/event/1'});
         }
-
-        /* event: {
-             name: "Angular js training",
-             date: "01/01/2017",
-             location: {
-                 city: "Bucharest",
-                 country: "Romania"
-             },
-             sessions: [
-                 {
-                     name: "Intro to angular",
-                     hour: "9:30",
-                     room: "Vlad Tepes",
-                     upvote: 0
-                 },
-                 {
-                     name: "Angular new features",
-                     hour: "12:30",
-                     room: "Mihai Viteazul",
-                     upvote: 0
-                 },
-                 {
-                     name: "Angular 1.5 new features",
-                     hour: "14:00",
-                     room: "Matei Basarab",
-                     upvote: 0
-                 }
-             ]
-         }*/
     }
 });
